af4b65e86b51f74a614414297857d1b16a2b0326
Ciré DIA <ciremoussadia@gmail.com>
2020-03-17 11:33:45 +0000

    db initial state

--- models.py
+++ models.py
@@ -3,25 +3,7 @@ from sqlalchemy import Column, String, Integer, DateTime
 from flask_sqlalchemy import SQLAlchemy
 
 database_path = os.environ['DATABASE_URL']
-
 db = SQLAlchemy()
-
-actors_movies = db.Table('actors_movies',
-  db.Column('actor_id', db.Integer, db.ForeignKey('actor.id'), primary_key=True),
-  db.Column('movie_id', db.Integer, db.ForeignKey('movie.id'), primary_key=True)
-)
-
-class Movie(db.Model):
-  id = Column(Integer, primary_key=True)
-  title = Column(String, unique=True, nullable=False)
-  release = Column(DateTime, nullable=False)
-  actors = db.relationship('Actor', secondary=actors_movies, lazy='subquery', backref=db.backref('movies'))
-
-class Actor(db.Model):
-  id = Column(Integer, primary_key=True)
-  name = Column(String, unique=True, nullable=False)
-  age = Column(String, nullable=False)
-  gender = Column(String, nullable=False)
 
 
 def setup_db(app, database_path=database_path):
@@ -30,3 +12,22 @@ def setup_db(app, database_path=database_path):
   db.app = app
   db.init_app(app)
   db.create_all()
+
+actors_movies = db.Table('actors_movies',
+  db.Column('actor_id', db.Integer, db.ForeignKey('actor.id'), primary_key=True),
+  db.Column('movie_id', db.Integer, db.ForeignKey('movie.id'), primary_key=True)
+)
+class Actor(db.Model):
+  id = Column(Integer, primary_key=True)
+  name = Column(String, unique=True, nullable=False)
+  age = Column(String, nullable=False)
+  gender = Column(String, nullable=False)
+
+
+class Movie(db.Model):
+  id = Column(Integer, primary_key=True)
+  title = Column(String, unique=True, nullable=False)
+  release = Column(DateTime, nullable=False)
+  actors = db.relationship('Actor', secondary=actors_movies, lazy='subquery', backref=db.backref('movies'))
+
+  
